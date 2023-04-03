f62d472df9ae6a75726ea507a23b639b089f94c1
BakiJetishi <105597452+BakiJetishi@users.noreply.github.com>
2023-04-03 17:18:17 +0200

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,3 @@
 export const API_URL = 'https://api.themoviedb.org/';
-export const API_KEY = '?api_key=7a246e3a205d325cca05db9d16adc84c'
+export const API_KEY = '' // Put your API KEY
 export const RES_PER_PAGE = 8;
